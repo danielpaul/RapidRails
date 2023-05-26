7d1bc3761a1cae6d905df8d3c583784c792ca6af
Alan Biju <alanbiju79@gmail.com>
2023-05-26 17:19:27 +0100

    update confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  devise_for :users, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: {
+    registrations: "registrations",
+    confirmations: 'confirmations'
+  }
 
   scope controller: :static do
     get :confirm_email, path: "users/confirm-email"
