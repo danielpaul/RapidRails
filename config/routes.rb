838a84f259fec902bd6352ea2efc760a4d9edf9b
Daniel Paul <im@danielpaul.me>
2023-05-30 21:14:04 +0100

    cleaned up PR

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  # ---------- [ Devise ] ---------- #
   devise_for :users, controllers: {registrations: "registrations"}
 
   devise_scope :user do
     get :confirm_email, to: "registrations#confirm_email", as: "confirm_email", path: "users/confirm-email"
   end
 
-  scope controller: :static do
-  end
+  
 end
