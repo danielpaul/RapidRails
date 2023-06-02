ca8bcf3d433f9cea9083e29d347044bf81834628
Daniel Paul <im@danielpaul.me>
2023-06-02 09:05:44 +0100

    active classes

--- app/views/components/app_sidebar_menu_component.rb
+++ app/views/components/app_sidebar_menu_component.rb
@@ -82,16 +82,21 @@ class AppSidebarMenuComponent < ApplicationComponent
             if menu_item[:items] && menu_item[:items].any?
               ul class: "-mx-2 space-y-1 #{menu_item[:item_menu_classes]}", role: 'list' do
                 menu_item[:items].each do |item|
+                  li_classes = 'text-gray-700 dark:text-gray-300 hover:text-primary hover:bg-gray-50 dark:hover:bg-card-dark group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                  icon_classes = 'h-6 w-6 shrink-0 text-gray-400 dark:text-gray-300 group-hover:text-primary'
+
+                  if item[:active]
+                    li_classes += ' bg-gray-50 dark:bg-card-dark text-primary'
+                    icon_classes += ' text-primary'
+                  end
+
                   li do
                     link_to(
                       item[:path],
-                      class: 'text-gray-700 dark:text-gray-300 hover:text-primary hover:bg-gray-50 dark:hover:bg-card-dark group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                      class: li_classes
                     ) do
                       if item[:icon]
-                        unsafe_raw heroicon(
-                          item[:icon],
-                          options: { class: 'h-6 w-6 shrink-0 text-gray-400 dark:text-gray-300 group-hover:text-primary' }
-                        )
+                        unsafe_raw heroicon(item[:icon], options: { class: icon_classes })
                       else
                         span class: 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border font-medium bg-white text-gray-400 border-gray-200 group-hover:border-primary group-hover:text-primary text-[0.625rem]' do
                           item[:title][0]
