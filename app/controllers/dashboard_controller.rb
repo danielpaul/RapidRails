f932b4a52186a746e9f37d36cb51b6366da0b9cf
Daniel Paul <im@danielpaul.me>
2023-06-02 09:57:02 +0100

    dashboard and other cards work nicely

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    flash_message(
+      :success,
+      'Welcome to Rails Starter!',
+      'Hope that this starter template gets your project up and running in no time.', now: true
+    )
   end
 end
