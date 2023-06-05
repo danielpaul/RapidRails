ba9b8b56a0573933817662d3c7531faa1b54dd66
Daniel Paul <im@danielpaul.me>
2023-06-05 12:32:22 +0100

    set default tiemzone

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module RailsStarter
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    
+    config.time_zone = "UTC"
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
