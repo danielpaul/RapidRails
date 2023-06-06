882e8ee46bb988fc0cba36b3027ded4f90f0286a
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-06 16:56:38 +0100

    mailer layout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -28,9 +28,9 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-
+  # config.mailer.layout = 'mailer'
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
