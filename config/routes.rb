a50bf4bb99d2b360bab934416a8fa8d3618c8d0d
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-06 09:50:01 +0100

    letter opener web added

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,10 @@ Rails.application.routes.draw do
 
   # ---------- [ Gems ] ---------- #
   mount ForestLiana::Engine => '/forest'
+
+
+  if Rails.env.development? || Rails.env.test?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
+
 end
