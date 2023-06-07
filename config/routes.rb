c847e676937644e92685d987566eba3422710dd6
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-07 18:41:00 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   
   get '/404', to: "errors#not_found"
   get '/500', to: "errors#internal_server"
+  
   # ---------- [ Gems ] ---------- #
   mount ForestLiana::Engine => '/forest'
 end
