0f4a0910b6ea38a01627467b95fb14f107d7acb2
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-09 17:58:44 +0100

    unlock remo

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -6,11 +6,7 @@ class DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "faketoken")
   end
-
-  def unlock_instructions
-    Devise::Mailer.unlock_instructions(User.first, "faketoken")
-  end
-
+  
   def email_changed
     Devise::Mailer.email_changed(User.first)
   end
