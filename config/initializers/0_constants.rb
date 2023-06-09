b19075f707ae13fb35afe370a705cef2cc74cc5f
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-09 11:49:29 +0100

    fixes

--- config/initializers/0_constants.rb
+++ config/initializers/0_constants.rb
@@ -2,6 +2,5 @@
 
 APP_NAME = "Example App"
 COMPANY_NAME = "Example Company, Inc."
-COPYRIGHT_DATE = "2020"
 DEFAULT_FROM_EMAIL_ONLY = "team@example.com"
 DEFAULT_FROM_EMAIL = "#{APP_NAME} <#{DEFAULT_FROM_EMAIL_ONLY}>"
