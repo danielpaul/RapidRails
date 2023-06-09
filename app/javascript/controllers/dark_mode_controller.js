23dcd022c06a66f2aa2182d7720df408f1d5380f
jacobperia <jacob@identitysquare.com>
2023-06-09 12:20:38 -0400

    added stimulus controller

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,5 +1,21 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect () {}
+  connect () {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }
+
+  darkMode () {
+    localStorage.theme = 'dark'
+    document.documentElement.classList.add('dark')
+  }
+
+  lightMode () {
+    localStorage.theme = 'light'
+    document.documentElement.classList.remove('dark')
+  }
 }
