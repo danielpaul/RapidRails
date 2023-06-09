281d9dba3de790cdbfd90ec1da0767f74ef92fc1
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-06-09 18:02:20 +0100

    Revert "unlock remo"
    This reverts commit 99adfb1e3f0cc8e3e034fe22c1d426ff12f2a3e7.

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -7,7 +7,11 @@ class DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "faketoken")
   end
-  
+
+  def unlock_instructions
+    Devise::Mailer.unlock_instructions(User.first, "faketoken")
+  end
+
   def email_changed
     Devise::Mailer.email_changed(User.first)
   end
