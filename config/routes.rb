e78e8f2d8ff33e4c622d0eac2b28ad0a44819a9a
Daniel Paul <im@danielpaul.me>
2023-06-10 21:19:33 +0100

    routes file and disable

--- config/routes.rb
+++ config/routes.rb
@@ -24,20 +24,7 @@ Rails.application.routes.draw do
   get "/500", to: "errors#internal_server"
 
   # ---------- [ API Routes ] ---------- #
-  namespace :api do
-    namespace :v1 do
-      # ---------- [ Auth Controller ] ---------- #
-      namespace :auth do
-        post "/sign_in", to: "auth#sign_in"
-        post "/forgot_password", to: "auth#forgot_password"
-        post "/resend_invite", to: "auth#forgot_password"
-        post "/confirm_email", to: "auth#confirm_email"
-        post "/extend_token", to: "auth#extend_token"
-        get "/user", to: "auth#user"
-        put "/user", to: "auth#update_user"
-      end
-    end
-  end
+  draw :api if ENABLE_API == true
 
   # ---------- [ Gems ] ---------- #
   mount ForestLiana::Engine => "/forest"
--- db/schema.rb
+++ db/schema.rb
@@ -56,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_173836) do
     t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
+
 end
