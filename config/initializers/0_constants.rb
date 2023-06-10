6b8dc9df6f5416f7a3e45f2a430ca2756a92fc1b
Daniel Paul <im@danielpaul.me>
2023-06-10 10:02:08 +0100

    sentry updates

--- config/initializers/0_constants.rb
+++ config/initializers/0_constants.rb
@@ -1,11 +1,10 @@
 # File is prefixed with 0_ so that it loads first
 
-APP_NAME = "Rails Starter"
+APP_NAME = "Rails Starter".freeze
 
-DEFAULT_FROM_EMAIL_ONLY = "team@example.com"
-DEFAULT_FROM_EMAIL = "#{APP_NAME} <#{DEFAULT_FROM_EMAIL_ONLY}>"
+DEFAULT_FROM_EMAIL_ONLY = "team@example.com".freeze
+DEFAULT_FROM_EMAIL = "#{APP_NAME} <#{DEFAULT_FROM_EMAIL_ONLY}>".freeze
 
-SENTRY_DSN_RAILS = "XXX"
-SENTRY_DSN_JS = "XXX"
-
-ENABLE_SENTRY = false
+ENABLE_SENTRY = Rails.env.production? || Rails.env.staging?
+SENTRY_DSN_RAILS = "XXX".freeze
+SENTRY_DSN_JS = "XXX".freeze
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,15 +1,25 @@
 Sentry.init do |config|
   # DSN is safe to store here and be visible to the public
   # https://docs.sentry.io/product/sentry-basics/dsn-explainer/
-  config.dsn = SENTRY_DSN_RAILS
+
+  # enabled_environments
+  # unset the SENTRY_DSN variable to disable Sentry
+  config.dsn = ENABLE_SENTRY ? SENTRY_DSN_RAILS : nil
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
-  config.traces_sample_rate = 0.25
+  # nil (default) or 0.0 means the tracing feature is disabled.
+  # If production, 0.1, staging 100%
+  config.traces_sample_rate = Rails.env.production? ? 0.1 : 1.0
 
-  # Sentry set to work only in production environment
-  config.enabled_environments = %w[production]
+  # When its value is false (default), sensitive information like
+  # user ip, user cookie, request body, query string in the url
+  # will not be sent to Sentry.
+  config.send_default_pii = false
+
+  # more config options:
+  # https://docs.sentry.io/platforms/ruby/guides/rails/configuration/options/
 end
