83e1140d6b520b1207fb27cd90057dcece18613f
Daniel Paul <im@danielpaul.me>
2023-06-10 19:26:17 +0100

    footer component in phlex & live reload on phlex compent update

---
+++ config/initializers/rails_live_reload.rb
@@ -0,0 +1,16 @@
+RailsLiveReload.configure do |config|
+  # config.url = "/rails/live/reload"
+
+  # Default watched folders & files
+  # config.watch %r{app/views/.+\.(erb|haml|slim)$}
+  # config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts|jsx)).*}, reload: :always
+
+  # More examples:
+  # config.watch %r{app/helpers/.+\.rb}, reload: :always
+  # config.watch %r{config/locales/.+\.yml}, reload: :always
+
+  # Watch Phlex components
+  config.watch %r{app/views/components/.+\.(rb)$}, reload: :always
+
+  # config.enabled = Rails.env.development?
+end if defined?(RailsLiveReload)
