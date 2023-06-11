39fdd872fc531273069531341da9173388c85e55
Daniel Paul <im@danielpaul.me>
2023-06-11 15:25:33 +0100

    updates

--- app/services/anonymization_service.rb
+++ app/services/anonymization_service.rb
@@ -1,37 +1,39 @@
-# app/services/anonymization_service.rb
 class AnonymizationService
   def self.anonymize_user(user_id)
-    user = User.find_by(id: user_id)
+    user = User.find(user_id)
 
     return if user.nil? || user.anonymized_at.present?
 
-    user.full_name = "Deleted User"
-    user.email = generate_anonymized_email(user.id)
-    user.save
-    user.confirm
-    user.password = SecureRandom.hex(10)
-    user.current_sign_in_ip = nil
-    user.last_sign_in_ip = nil
-    user.unconfirmed_email = nil
-    user.reset_password_token = nil
-    user.confirmation_token = nil
-    user.anonymized_at = Time.current
-    user.save
+    user.assign_attributes(
+      full_name: "Deleted User",
+
+      email: generate_anonymized_email(user.id),
+      unconfirmed_email: nil,
+      password: Devise.friendly_token[0, 20],
+
+      current_sign_in_ip: nil,
+      last_sign_in_ip: nil,
+
+      reset_password_token: nil,
+      confirmation_token: nil,
+
+      anonymized_at: Time.current
+    )
+
+    user.save(validate: false)
   end
 
   private
 
-  def self.generate_anonymized_email(user_id)
-    email = "#{user_id}@deleted.example.com"
-
+  def self.generate_anonymized_email(prefix, domain = "deleted.example.com")
+    email = "#{prefix}@#{domain}"
+    counter = 1
+  
     while User.exists?(email: email)
-      email = "#{user_id}+#{generate_random_number}@deleted.example.com"
+      email = "#{prefix}+#{counter}@#{domain}"
+      counter += 1
     end
-
+  
     email
-  end
-
-  def self.generate_random_number
-    rand(1..9999)
-  end
+  end  
 end
--- app/workers/anonymization_worker.rb
+++
@@ -1,9 +0,0 @@
-# app/workers/anonymization_worker.rb
-
-class AnonymizationWorker
-  include Sidekiq::Worker
-
-  def perform(user_id)
-    AnonymizationService.anonymize_user(user_id)
-  end
-end
