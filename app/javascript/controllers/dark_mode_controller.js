a64bd80b55eaf8ae8fb1f4485dfed02d9e15d6a5
Daniel Paul <im@danielpaul.me>
2023-06-14 13:30:39 +0100

    use cookies instaead of localstorage for theme

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from '@hotwired/stimulus'
+import Cookies from 'js-cookie'
 
 export default class extends Controller {
-  connect () {
+  connect() {
     if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
+      Cookies.get('theme') === 'dark' ||
+      (!Cookies.get('theme') &&
         window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
       document.documentElement.classList.add('dark')
@@ -13,13 +14,13 @@ export default class extends Controller {
     }
   }
 
-  darkMode () {
-    localStorage.theme = 'dark'
+  darkMode() {
+    Cookies.set('theme', 'dark', { expires: 7 })
     document.documentElement.classList.add('dark')
   }
 
-  lightMode () {
-    localStorage.theme = 'light'
+  lightMode() {
+    Cookies.set('theme', 'light', { expires: 7 })
     document.documentElement.classList.remove('dark')
   }
 }
