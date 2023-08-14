13839d825a511d76b299d7dc90f96b6e11b2f0d3
jacobperia <79951942+jacobperia@users.noreply.github.com>
2023-08-14 18:54:23 -0400

    remove comment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,3 @@
-# Perform Sidekiq jobs immediately in development,
-# so you don't have to run a separate process.
-# Also benefit from code reloading.
-
 if Rails.env.test?
   require "sidekiq/testing"
   Sidekiq::Testing.inline!
