fd1dbf037337716baad3a3596a978ffcd7a8962c
Alan Biju <alanbiju79@gmail.com>
2023-10-25 12:36:18 +0100

    add production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,10 @@ Rails.application.configure do
   config.action_mailer.postmark_settings = {
     api_token: Rails.application.credentials.dig(Rails.env.to_sym, :postmark_api_token)
   }
+  config.action_mailer.default_url_options = {host: Rails.application.credentials.dig(Rails.env.to_sym, :host)}
+  # Mailer asset host
+  config.action_mailer.asset_host = Rails.application.credentials.dig(Rails.env.to_sym, :host)
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
