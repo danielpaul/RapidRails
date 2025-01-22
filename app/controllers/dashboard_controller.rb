adc58cfac6e074cc4fafefddd84245391337f2de
Daniel Paul <im@danielpaul.me>
2025-01-22 12:47:56 +0530

    Update dashboard_controller.rb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,20 +16,20 @@ class DashboardController < ApplicationController
     flash_message(
       :notice,
       "This is a notice message.",
-      "This is a notice message. That is really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam a orci eu tristique. Suspendisse sit amet porttitor erat. Mauris luctus lacus eleifend ante fermentum finibus in ut odio. Proin vitae massa nisi. Praesent pharetra a nisl vitae dapibus. Nunc tempor elit vel leo suscipit, iaculis placerat nibh hendrerit. ",
       now: true
     )
 
     flash_message(
       :error,
       "This is an error message.",
-      "This is an error message. That is really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long.",
+      "Quisque at velit faucibus, euismod risus eget, semper libero. Praesent sit amet dolor leo. Mauris volutpat, libero non porttitor elementum, leo sem lacinia metus, vitae bibendum tellus mi non ipsum.",
       now: true
     )
 
     flash.now[:notice] = "This is an notice message."
     flash.now[:success] =
-      "This is an alert message. That is really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really really long."
+      "Fusce elementum porta ornare. Phasellus in ultricies nunc. Curabitur auctor urna sed erat consequat malesuada. Nunc nec lacus dapibus, hendrerit justo in, commodo neque. Ut malesuada nunc magna, vel eleifend sapien viverra nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
     flash.now[:error] = "This is an error message."
   end
 end
