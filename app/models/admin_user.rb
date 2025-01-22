f5c909f03d3d3a04b809afb13e4848d796e17880
Daniel Paul <im@danielpaul.me>
2025-01-22 16:12:03 +0530

    Update admin_user.rb

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,8 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  def email_name_greeting
+    "Admin (#{email})"
+  end
 end
