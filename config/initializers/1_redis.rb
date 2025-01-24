937ed2015a5086dc7f8565cd8f1b0373eb790715
Daniel Paul <im@danielpaul.me>
2025-01-25 02:06:41 +0530

    redis order fix

---
+++ config/initializers/1_redis.rb
@@ -0,0 +1,9 @@
+REDIS_URL = ENV["REDIS_URL"] || ENV["REDISCLOUD_URL"] || "redis://localhost:6379/0"
+
+REDIS_CONFIG = {
+  url: REDIS_URL
+}
+
+if Rails.env.production? || Rails.env.staging?
+  REDIS_CONFIG[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+end
