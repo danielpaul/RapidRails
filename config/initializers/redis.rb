4a3e2293569352118fb5b84342e6ca35481e2bc9
Daniel Paul <im@danielpaul.me>
2025-01-24 22:30:49 +0530

    redis config for heroku kv

---
+++ config/initializers/redis.rb
@@ -0,0 +1,1 @@
+$redis = Redis.new(url: ENV["REDIS_URL"] || ENV["REDISCLOUD_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,3 +2,17 @@ if Rails.env.test?
   require "sidekiq/testing"
   Sidekiq::Testing.inline!
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV["REDIS_URL"] || ENV["REDISCLOUD_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+      url: ENV["REDIS_URL"] || ENV["REDISCLOUD_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
