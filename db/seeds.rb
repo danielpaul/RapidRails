7947738f1e9ec78a1b1ae02a0faaa1702ef1105f
Daniel Paul <333233+danielpaul@users.noreply.github.com>
2025-07-08 15:08:37 +0100

    Seed admin user confirmed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+if Rails.env.development?
+  admin = AdminUser.find_or_initialize_by(email: "admin@example.com")
+  admin.password = "password"
+  admin.password_confirmation = "password"
+  admin.skip_confirmation! if admin.confirmed_at.blank?
+  admin.save!
+end
